1f8e719bd4bcfee85bb45756d88d74b8d2db07ac
dogan can selek <dogancan.selek@for.com.tr>
2022-12-06 18:22:05 +0300

    update dashboard for adding get user service

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
 
 function Dashboard() {
+
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        let token = localStorage.getItem('token');
+        async function fetchUser() {
+            let response = await fetch(`http://localhost:5000/user?token=${token}`);
+            let data = await response.json();
+            setData(data);                   
+        }
+
+        fetchUser();
+    }, []);
   return (
-    <div>Dashboard</div>
+    <div>{data.bio}</div>
   )
 }
 
