64ef65ec17ebd6d3f1e474755d4f5911c7e1a607
dogan can selek <dogancan.selek@for.com.tr>
2022-12-06 17:34:56 +0300

    update serverjs for setting auth

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
 let express = require('express');
 let cors = require('cors');
+let axios = require('axios');
 const bodyParser = require('body-parser');
 let app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+let client_secret = "d5e1e0a853d069a11548dadbad7850db2c1891b9";
+let client_id = "b2fa34376e313b7517ec";
+
+app.get('/auth', (req, res) => {
+    let code = req.query.code;
+    axios.post('https://github.com/login/oauth/access_token',{
+        client_id: client_id,
+        client_secret: client_secret,
+        code: code
+    },{
+        headers: {
+            accept: 'application/json'
+        }
+    }).then((response) => {
+        res.send(response.data);
+    }
+    ).catch((error) => {
+        console.log(error);
+    })
+});
+
+app.listen(5000, () => {
+    console.log('Server started on port 5000');
+});
