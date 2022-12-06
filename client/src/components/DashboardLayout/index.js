f3ba2a6faecbd260654e57db1f72f001db94bed8
dogan can selek <dogancan.selek@for.com.tr>
2022-12-07 02:29:42 +0300

    update for styling

--- client/src/components/DashboardLayout/index.js
+++ client/src/components/DashboardLayout/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function DashboardLayout({children}) {
   return (
-    <div className='flex flex-col w-2/3 h-screen border-x-2 border-[#c9510c] '>
+    <div className='dashboard flex flex-col w-2/3 h-screen overflow-auto border-x-2 border-[#c9510c]'>
         {children}
     </div>
   )
