c56537985f476f5f0f6f60ce9aa4d13cbbd93552
dogan can selek <dogancan.selek@for.com.tr>
2022-12-07 02:29:30 +0300

    update server for adding repos and commits services

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(bodyParser.json());
 
 let client_secret = "d5e1e0a853d069a11548dadbad7850db2c1891b9";
 let client_id = "b2fa34376e313b7517ec";
+let api = "https://api.github.com";
 
 app.get('/auth', (req, res) => {
     let code = req.query.code;
@@ -29,7 +30,7 @@ app.get('/auth', (req, res) => {
 
 app.get('/user', (req, res) => {
     let token = req.query.token;
-    axios.get('https://api.github.com/user',{
+    axios.get(`${api}/user`,{
         headers: {
             Authorization: `Bearer ${token}`
         }
@@ -44,7 +45,23 @@ app.get('/repos', (req, res) => {
     let token = req.query.token;
     let username = req.query.username;
     let per_page = req.query.per_page;
-    axios.get(`https://api.github.com/users/${username}/repos?per_page=${per_page}`,{
+    axios.get(`${api}/users/${username}/repos?per_page=${per_page}&page=2`,{
+        headers: {
+            Authorization: `Bearer ${token}`,
+            accept: 'application/vnd.github.v3+json'
+        }
+    }).then((response) => {
+        res.send(response.data);
+    }).catch((error) => {
+        res
+    })
+})
+
+app.get('/commits', (req, res) => {
+    let token = req.query.token;
+    let owner = req.query.owner;
+    let repo = req.query.repo;
+    axios.get(`${api}/repos/${owner}/${repo}/commits`,{
         headers: {
             Authorization: `Bearer ${token}`,
             accept: 'application/vnd.github.v3+json'
