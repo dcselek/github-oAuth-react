f5ea6be0dd9094d182b88226661bede1c75dfd0d
dogan can selek <dogancan.selek@for.com.tr>
2022-12-06 17:35:40 +0300

    update app for logining

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
-import {Routes, Route, Outlet} from 'react-router-dom';
+import { Routes, Route, Outlet, Navigate } from 'react-router-dom';
 import LoginButton from './components/LoginButton';
 import Dashboard from './pages/Dashboard';
 import './App.css';
+import { useEffect } from 'react';
+import { useAuth } from './Context/authContext';
+import ProtectedRoute from './components/ProtectedRoute';
+import AuthProvider from './Context/authContext';
 
 function App() {
+
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="dashboard" element={<Dashboard />} />
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </div>
   );
 }
@@ -19,9 +26,27 @@ function App() {
 export default App;
 
 function Home() {
+
+  const { token } = useAuth();
+
+  useEffect(() => {
+    let code = new URLSearchParams(window.location.search).get('code');
+    if (code && token === null) {
+      async function fetchToken() {
+        let response = await fetch(`http://localhost:5000/auth?code=${code}`);
+        let data = await response.json();
+        localStorage.setItem('token', data.access_token);
+        window.history.pushState({}, null, '/');
+      }
+
+      fetchToken();
+    }
+  }, []);
+
   return (
     <div>
       <LoginButton />
+      {token && <Navigate to="/dashboard" />}
     </div>
   );
 }
