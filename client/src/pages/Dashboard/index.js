7041c31c7877d0f97b556957765f58810321184b
dogan can selek <canselek2434@gmail.com>
2022-12-07 03:31:11 +0300

    update for pagination

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import DashboardLayout from '../../components/DashboardLayout';
+import Pagination from '../../components/Pagination';
 import RepoTable from '../../components/RepoTable';
 
 
@@ -7,8 +8,7 @@ function Dashboard() {
 
     const [userData, setUserData] = useState(null);
     const [repos, setRepos] = useState(null);
-
-
+    const [currentPage, setCurrentPage] = useState(1);
 
     useEffect(() => {
         const fetchUser = async () => {
@@ -17,7 +17,7 @@ function Dashboard() {
             let data = await response.json();
             setUserData(data);
 
-            if(response?.status === 200){
+            if (response?.status === 200) {
                 fetchRepos(data.login);
             }
         }
@@ -33,13 +33,32 @@ function Dashboard() {
 
     }, []);
 
+    useEffect(() => {
+        const fetchRepos = async (login) => {
+            let token = localStorage.getItem('token');
+            let response = await fetch(`http://localhost:5000/repos?token=${token}&username=${login}&per_page=10&page=${currentPage}`);
+            let data = await response.json();
+            if(data.length > 0){
+                setRepos(data);
+            }
+        }
+
+        if (userData !== null || currentPage !== 1) {
+            fetchRepos(userData.login);
+        }
+    }, [currentPage, userData]);
+
+    function setPaginate(pageNumber) {
+        setCurrentPage(pageNumber);
+    }
+
     if (userData === null || repos === null) {
         return <div>Loading...</div>
     } else {
         return (
             <DashboardLayout>
                 <div className="w-1/3">
-                    <div className="flex items-center justify-center ml-[-1px] p-4 border-l-0 border border-[#c9510c] bg-[#bd2c00]">
+                    <div className="flex items-center justify-center ml-[-1px] p-4 border-l-0 border border-[#c9510c] bg-gray-800 text-gray-300 hover:bg-gray-700 hover:text-white">
                         <img src={userData.avatar_url} alt="avatar" className="w-40 h-40 rounded-full" />
                         <div className='flex flex-col'>
                             <h1 className="text-2xl font-bold">{userData.name}</h1>
@@ -55,7 +74,7 @@ function Dashboard() {
                 <div className='mt-16'>
                     <div className="flex flex-col items-center justify-center">
                         <h1 className="text-2xl font-bold">Repositories</h1>
-                        <div className="flex flex-col items-center justify-center">
+                        <div className="flex flex-col items-center justify-center mt-6">
                             <table className="table-auto">
                                 <thead>
                                     <tr>
@@ -66,11 +85,12 @@ function Dashboard() {
                                         <th className="px-4 py-2">Download</th>
                                     </tr>
                                 </thead>
-                                {repos.map((repo, key) => 
+                                {repos.map((repo, key) =>
                                     <RepoTable key={key} name={repo.name} owner={repo.owner.login} isPrivate={repo.private} lastRelease={new Date(repo.updated_at).toString()} downloadUrl={repo.downloads_url} />
                                 )}
                             </table>
                         </div>
+                        <Pagination page={currentPage} setPage={setPaginate} />
                     </div>
                 </div>
             </DashboardLayout>
